4f4494eda783932e72a71591f6a41e4cbc03b4e0
IsaacAHS <isaac.huamani@pucp.edu.pe>
2024-08-28 19:36:10 -0500

    Lab 2

---
+++ Lab2_20215421/src/test/java/com/example/lab2_20215421/Lab220215421ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab2_20215421;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab220215421ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
